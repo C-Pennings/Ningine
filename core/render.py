41f174dad0190147dd6532b033849f68ee4ac917
C-pennings <charliepennings13@gmail.com>
2025-11-12 13:06:00 -0500

    Setting up Render and Window object

---
+++ core/render.py
@@ -0,0 +1,23 @@
+import pygame
+import sys
+import moderngl
+
+class Renderer: #uses moderngl to render objects
+    def __init__(self):
+        #setup render object
+        pass
+
+    def add(self):
+        #add render objects to queue
+        pass
+
+    def render(self):
+        #sort objects by z value and render them
+        pass
+
+class Window: #used to define a window
+    def __init__(self):
+        pass
+
+    def get_window(self):
+        pass
