85b27bd753962393e517cbfe0d33253bb8749269
C-pennings <charliepennings13@gmail.com>
2025-11-19 18:33:55 -0500

    updated the current test program

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 # main.py
 import pygame
+from core.render_objects import CubeMesh
 from core.render import Renderer
 
 pygame.init()
 renderer = Renderer()
 
-while True:
+# Create one cube
+cube = CubeMesh(renderer.ctx)
+renderer.meshes = [cube]  # We'll make this cleaner later
+
+clock = pygame.time.Clock()
+running = True
+
+while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit(); quit()
-            
-    renderer.begin_frame(None)      # camera not ready yet
-    renderer.draw_scene(None)       # scene not ready yet
+            running = False
+
+    renderer.begin_frame()
+    for mesh in renderer.meshes:
+        mesh.draw()
     renderer.end_frame()
+    clock.tick(60)
+
+pygame.quit()
